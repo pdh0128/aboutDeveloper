23837cdb75fe2c33bc8522e035f225652cd5fac3
pdh0128 <pdh0128a@gmail.com>
2024-11-27 15:53:15 +0900

    질문에 대한 응답 오류 줄이기

--- Router.py
+++ Router.py
@@ -25,12 +25,12 @@ tools = [
     Tool(
         name="CSV Agent",
         func=csv_agent_executor,
-        description="개발자들에 대한 정보를 참조하고싶을때 유용해."
+        description="개발자와 관련된 정보(공부 방법, 기술, 이력 등)에 대한 질문에 사용합니다."
     )
 ]
 
 base_prompt = hub.pull("langchain-ai/react-agent-template")
-promps = base_prompt.partial(instructions="")
+promps = base_prompt.partial(instructions="CSV Agent는 개발자 정보에 대한 질문에 사용되어야 하며, 그 외에는 Python Agent를 사용하십시오. 개발자 혹은 개발과 연관이 없는 질문에는 '모른다'고 응답하세요.")
 
 control_agent = create_react_agent(
     prompt=promps,
@@ -40,7 +40,7 @@ control_agent = create_react_agent(
 
 control_agent_executor = AgentExecutor(agent=control_agent, tools=tools, verbose=True)
 
-input_query = "개발자들 기술스택 알려줘"
+input_query = "개발자들은 보통 어떤 방식으로 공부해?"
 
 res = control_agent_executor.invoke(input={"input": input_query})
 
